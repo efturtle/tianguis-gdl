5e48a4ff2112cfb3d2b3e7c07bb0aab3f043c91d
Beto Cuevas <46721097+efturtle@users.noreply.github.com>
2024-02-14 00:01:14 -0600

    removes migrations

--- database/migrations/20240201-add-tianguis-table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE tianguis (
-	id UUID PRIMARY KEY,
-	nombre varchar(255) NOT NULL,
-	ciudad varchar(255),
-	empieza varchar(255) NOT NULL,
-	termina varchar(255) NOT NULL,
-	fecha_creada timestamp DEFAULT (datetime('now')),
-	fecha_actualizada timestamp DEFAULT (datetime('now'))
-);
